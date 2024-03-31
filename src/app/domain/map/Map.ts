26e7f2413967af0d45a1195bcd1507e7b3883333
Alina-Pishak <alinapishak@icloud.com>
2024-03-31 22:56:38 +0300

    add return data

--- src/app/domain/map/Map.ts
+++ src/app/domain/map/Map.ts
@@ -1,4 +1,10 @@
-import { JsonController, Post, UploadedFile } from "routing-controllers";
+import {
+  JsonController,
+  Post,
+  UploadedFile,
+  Res,
+  Req,
+} from "routing-controllers";
 import fs from "fs";
 import path from "path";
 import unzipper from "unzipper";
@@ -15,8 +21,10 @@ export default class Map {
     file: Express.Multer.File
   ) {
     try {
-      if (!file) {
-        return new ApiError(400, { message: "No ZIP file uploaded" });
+      if (!file || file.mimetype !== "application/zip") {
+        return new ApiError(400, {
+          message: "File must be in zip archive format",
+        });
       }
 
       const zipFilePath = path.join("public/zips", file.originalname);
@@ -30,12 +38,10 @@ export default class Map {
         .promise();
       const files = await fs.promises.readdir(extractPath);
       const filePath = path.join(extractPath, files[0]);
-      const fileData = await fs.promises.readFile(filePath);
+      const data = await fs.promises.readFile(filePath);
       fs.unlinkSync(zipFilePath);
       fs.unlinkSync(filePath);
-      return new ApiResponse(true, {
-        message: "ZIP file processed successfully",
-      });
+      return new ApiResponse(true, data, " ZIP file processed successfully");
     } catch (error) {
       return new ApiError(500, { message: "Error processing ZIP file" });
     }
